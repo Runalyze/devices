b65c5ff5d594bc0ddcaf870917c27d6fc066391a
Michael Pohl <michael@runalyze.com>
2020-06-13 14:43:57 +0200

    fix wahooo mapping

--- src/Mapping/WahooFitSdkMapping.php
+++ src/Mapping/WahooFitSdkMapping.php
@@ -23,7 +23,7 @@ class WahooFitSdkMapping
         return [
             28 => DeviceProfile::WAHOO_ELEMNT,
             31 => DeviceProfile::WAHOO_ELEMNT_BOLT,
-            39 => DeviceProfile::WAHOO_ELEMNT_ROAM,
+            37 => DeviceProfile::WAHOO_ELEMNT_ROAM,
         ];
     }
 
